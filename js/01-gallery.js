2ab8154ce73d30b0bd51d9072c5824b71204b409
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2023-01-24 16:35:42 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -55,17 +55,14 @@ function calculateTeamFinanceReport(salaries, team) {
   const salary = Object.values(salaries);
   const salaryWithTax = (salary, tax) =>
     Math.round(salary / (1 - Number.parseInt(tax) / 100));
-
   specialist.map(
     (el, index) => (specialityAndSalary[`${el}`] = salary[index].salary)
   );
-
   const amount = team.reduce((acc, el) => {
     return typeof acc[el.specialization] !== 'undefined'
       ? { ...acc, [el.specialization]: acc[el.specialization] + 1 }
       : { ...acc, [el.specialization]: 1 };
   }, {});
-
   let sum = 0;
   for (let speciality in specialityAndSalary) {
     report[`totalBudget${speciality}`] =
@@ -74,7 +71,6 @@ function calculateTeamFinanceReport(salaries, team) {
     sum += report[`totalBudget${speciality}`];
   }
   report['totalBudgetTeam'] = sum;
-
   return report;
 }
 
